b8508d4a474c5c8bc7be5981e489d684b61e6f26
Alabere-coder <alabere4real247@gmail.com>
2022-10-20 11:25:57 +0100

    About and changes

--- main.js
+++ main.js
@@ -27,6 +27,23 @@ faqs.forEach((faq) => {
 })
 
 
+// TO SHOW HIDE NAV MENU
+
+const menu = document.querySelector('.nav__menu');
+const openBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+openBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    openBtn.style.display = 'none';
+})
 
 
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'inline-block';
+}
 
+closeBtn.addEventListener('click', closeNav)
